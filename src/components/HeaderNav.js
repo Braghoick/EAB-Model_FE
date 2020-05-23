700042801c843f7dcbb35fde675fc8710d7a539f
braghoick <bjcr2726@gmail.com>
2020-05-22 21:19:05 -0600

    NavBar updated

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -2,22 +2,26 @@ import React from 'react';
 
 const HeaderNav = () =>{
 		return (
-				<div className="mb-3">
-					<ul className="nav">
-                              <li className="nav-item">
-                                   <a className="nav-link active" href="/  ">Active</a>
-                              </li>
-                              <li className="nav-item">
-                                   <a className="nav-link" href="/">Link</a>
-                              </li>
-                              <li className="nav-item">
-                                   <a className="nav-link" href="/">Link</a>
-                              </li>
-                              <li className="nav-item">
-                                   <a className="nav-link disabled" href="/" tabindex="-1" aria-disabled="true">Disabled</a>
-                              </li>
-                         </ul>
-				</div>
+               <nav className="navbar navbar-expand-lg navbar-light bg-light">
+               <a class="navbar-brand" href="/">
+                    <img src="https://getbootstrap.com/docs/4.4/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top mr-2" alt="" />
+                         Bootstrap
+               </a>
+               <div className="collapse navbar-collapse" id="navbarNav">
+                 <ul className="navbar-nav">
+                   <li className="nav-item active">
+                     <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                   </li>
+                   <li className="nav-item">
+                     <a className="nav-link" href="/">Features</a>
+                   </li>
+                   <li className="nav-item">
+                     <a class="nav-link" href="/">Pricing</a>
+                   </li>
+                   
+                 </ul>
+               </div>
+             </nav>
 
 		);
 	}
