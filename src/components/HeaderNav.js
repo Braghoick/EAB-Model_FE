ac762e7d4581bad62f172b8b33314d8b1ff49026
PuzzledCrib <luiscruzmiranda@hotmail.com>
2020-05-22 23:00:15 -0600

    Creacion de Modal
    Creacion de modal para prueba, falta integrar en el carrusel

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -1,20 +1,19 @@
 import React from 'react';
 
+// Aqui se crea el header nav para moverse entre las pantallas
+
 const HeaderNav = () =>{
 		return (
 				<div className="mb-3">
 					<ul className="nav">
                               <li className="nav-item">
-                                   <a className="nav-link active" href="/  ">Active</a>
+                                   <a className="nav-link active" href="#home">Home</a>
                               </li>
                               <li className="nav-item">
-                                   <a className="nav-link" href="/">Link</a>
+                                   <a className="nav-link" href="#mostViewed">Most Viewed</a>
                               </li>
                               <li className="nav-item">
-                                   <a className="nav-link" href="/">Link</a>
-                              </li>
-                              <li className="nav-item">
-                                   <a className="nav-link disabled" href="/" tabindex="-1" aria-disabled="true">Disabled</a>
+                                   <a className="nav-link" href="#aboutUs">About Us</a>
                               </li>
                          </ul>
 				</div>
