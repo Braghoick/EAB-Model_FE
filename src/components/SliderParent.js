63ff23f19b583a94a71fe12d4825c868f251f38d
yohel715 <yohel715@gmail.com>
2020-05-25 13:19:11 -0600

    Creación de la carpeta SCSS
    Creación de la carpeta SCSS, cambio de formatos de css  a scss y rutas de importación

--- src/components/SliderParent.js
+++ src/components/SliderParent.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Slider from "./Slider";
-import './SliderParent.css';
+import '../scss/SliderParent.scss';
 
 class SliderParent extends React.Component {
     render() {
