06c201d3852d800b978f5bd78bd81855faa94cf9
yohel715 <yohel715@gmail.com>
2020-05-24 22:58:52 -0600

    Correcion de Sliders

--- src/components/SliderParent.js
+++ src/components/SliderParent.js
@@ -5,37 +5,64 @@ import './SliderParent.css';
 class SliderParent extends React.Component {
     render() {
      let data = [
-        <div><img className="card-img" src="https://i.kinja-img.com/gawker-media/image/upload/t_original/nceu6qqumhzv8v52tmof.jpg"/>
+        <div>
+            <img className="card-img" alt=" " src="https://i.kinja-img.com/gawker-media/image/upload/t_original/nceu6qqumhzv8v52tmof.jpg" />
             <div className="card-body">
                 <h3 className="card-title">Titulo</h3>
                 <p className="card-text">Relleno</p>
             </div>
         </div>,
-         <div><img className="card-img" src="https://i.kinja-img.com/gawker-media/image/upload/t_original/ykm8sk7atmprf8v3pbpr.jpg"/>
+         <div>
+            <img className="card-img" alt=" " src="https://i.kinja-img.com/gawker-media/image/upload/t_original/ykm8sk7atmprf8v3pbpr.jpg" />
             <div className="card-body">
                 <h3 className="card-title">Titulo</h3>
                 <p className="card-text">Relleno</p>
             </div>
         </div>,
-         <div><img className="card-img" src="https://cnet2.cbsistatic.com/img/lSnyuIPjsqrc5URVmbSYNf2g0cw=/1200x675/2019/11/07/eafc3243-f819-4cf4-934f-73388b0906c8/rs-ford-mustang-mache-36.jpg"/>
+         <div>
+            <img className="card-img" alt=" " src="https://cnet2.cbsistatic.com/img/lSnyuIPjsqrc5URVmbSYNf2g0cw=/1200x675/2019/11/07/eafc3243-f819-4cf4-934f-73388b0906c8/rs-ford-mustang-mache-36.jpg" />
             <div className="card-body">
                 <h3 className="card-title">Titulo</h3>
                 <p className="card-text">Relleno</p>
             </div>
         </div>,
-         <div><img className="card-img" src="https://i.kinja-img.com/gawker-media/image/upload/t_original/ykm8sk7atmprf8v3pbpr.jpg"/>
+        <div>
+            <img className="card-img" alt=" " src="https://i.kinja-img.com/gawker-media/image/upload/t_original/ykm8sk7atmprf8v3pbpr.jpg"/>
             <div className="card-body">
                 <h3 className="card-title">Titulo</h3>
                 <p className="card-text">Relleno</p>
             </div>
         </div>,
-         <div><img className="card-img" src="https://i.kinja-img.com/gawker-media/image/upload/t_original/nceu6qqumhzv8v52tmof.jpg"/>
+         <div>
+            <img className="card-img" alt=" " src="https://i.kinja-img.com/gawker-media/image/upload/t_original/nceu6qqumhzv8v52tmof.jpg" />
             <div className="card-body">
                 <h3 className="card-title">Titulo</h3>
                 <p className="card-text">Relleno</p>
             </div>
         </div>,
-         <div><img className="card-img" src="https://cnet2.cbsistatic.com/img/lSnyuIPjsqrc5URVmbSYNf2g0cw=/1200x675/2019/11/07/eafc3243-f819-4cf4-934f-73388b0906c8/rs-ford-mustang-mache-36.jpg"/>
+         <div>
+            <img className="card-img" alt=" " src="https://cnet2.cbsistatic.com/img/lSnyuIPjsqrc5URVmbSYNf2g0cw=/1200x675/2019/11/07/eafc3243-f819-4cf4-934f-73388b0906c8/rs-ford-mustang-mache-36.jpg" />
+            <div className="card-body">
+                <h3 className="card-title">Titulo</h3>
+                <p className="card-text">Relleno</p>
+            </div>
+        </div>,
+         <div>
+            <img className="card-img" alt=" " src="https://i.kinja-img.com/gawker-media/image/upload/t_original/ykm8sk7atmprf8v3pbpr.jpg" />
+            <div className="card-body">
+                <h3 className="card-title">Titulo</h3>
+                <p className="card-text">Relleno</p>
+            </div>
+        </div>,
+         <div>
+            <img className="card-img" alt=" " src="https://i.kinja-img.com/gawker-media/image/upload/t_original/nceu6qqumhzv8v52tmof.jpg" />
+            <div className="card-body">
+                <h3 className="card-title">Titulo</h3>
+                <p className="card-text">Relleno</p>
+            </div>
+        </div>,
+         <div>
+            <img className="card-img" alt=" " src="https://cnet2.cbsistatic.com/img/lSnyuIPjsqrc5URVmbSYNf2g0cw=/1200x675/2019/11/07/eafc3243-f819-4cf4-934f-73388b0906c8/rs-ford-mustang-mache-36.jpg" />
             <div className="card-body">
                 <h3 className="card-title">Titulo</h3>
                 <p className="card-text">Relleno</p>
